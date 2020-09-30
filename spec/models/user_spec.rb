a6ec64ba582d0fd70c4d14206a3990e2e9650197
Diego Wins <diegow93@gmail.com>
2020-09-30 20:01:11 -0300

    Saving progress.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user_params) do
+    { email: 'test@test.com', password: '123456', username: 'test',
+      name: 'name', lastname: 'lastname', birth_date: '1/1/2000', address: '1st Street' }
+  end
+
+  describe 'User creation' do
+    it 'succeeds' do
+      User.create(user_params)
+      expect(User.count).to eq 1
+    end
+
+    context 'fails' do
+      before do
+        User.create(user_params)
+      end
+
+      it 'due to validations' do
+        expect { User.create!(user_params) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
