6c75b3aedf509a8a6fb8c28586b93efdf40b77d3
Valentín Segura <valentin.segura.h@gmail.com>
2020-10-02 20:17:03 -0300

    the relation between volunteer and type of document was modified

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,8 +1,8 @@
 class Volunteer < User
   validates :document_number, presence: true
-  validates :document_type_id, presence: true
+  validates :document_type, presence: true
 
-  has_one :document_type
+  belongs_to :document_type
 
   has_one_attached :document_face_pic
   has_one_attached :document_back_pic
