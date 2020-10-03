2db196d54668d34013de3256baff03e44452bf13
Mauro Pintado <m.pintado1994@gmail.com>
2020-10-03 20:24:35 -0300

    Order model fixes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
 class Order < ApplicationRecord            
     validates :title, presence: true, length: {minimum: 5}
-    validates :description, presence:true, length: {minimum: 5}
-    enum status: %i(created accepted inProcess finished cancelled)
+    validates :description, presence: true, length: {minimum: 5}
+    enum status: %i(created accepted in_process finished cancelled)
 
-    has_many :order_requests
+    has_many :order_requests, dependent: :destroy
     has_many :volunteers, :through => :order_requests
     
     belongs_to :helpee
