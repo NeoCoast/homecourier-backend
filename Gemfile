cad5680e1098f081742ee76ea0135b414d3bdc6b
Diego Wins <diegow93@gmail.com>
2020-10-11 13:30:38 -0300

    Subscription test for ActionCable.

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'rack-cors'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 4.0.1'
+  gem 'rspec-rails', '~> 3.9.1'
   gem 'simplecov', '~> 0.19.0'
   gem 'simplecov-html', '~> 0.12.3'
   gem 'factory_bot_rails', '~> 6.1.0'
--- spec/channels/web_notifications_channel_spec.rb
+++ spec/channels/web_notifications_channel_spec.rb
@@ -4,13 +4,11 @@ RSpec.describe WebNotificationsChannel, type: :channel do
   let!(:helpee) { create(:user, type: 'Helpee', confirmed_at: Faker::Date.between(from: 30.days.ago, to: Date.today)) }
 
   before do
-    # initialize connection with identifiers
-    stub_connection user_id: helpee.id
+    stub_connection current_user: helpee
   end
 
   it 'subscribes without streams' do
     subscribe
     expect(subscription).to be_confirmed
-    expect(subscription).not_to have_streams
   end
 end
