3e19a02e58f87520d0f3c8d4135d8b721bb7be69
Andrés Collares <andres.collares@fing.edu.uy>
2020-10-16 20:31:12 -0300

    add test and demo environments to rack cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'localhost:8080', 'test-homecourier-backend.herokuapp.com', 'demo-homecourier-backend.herokuapp.com'
 
     resource '*',
              headers: :any,
