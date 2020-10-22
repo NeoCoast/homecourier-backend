8516f628dc779103a7c502165bcbfa484cb15de2
Diego Wins <diegow93@gmail.com>
2020-10-22 12:53:14 -0300

    Registration controllers refactor.

---
+++ app/controllers/api/v1/devise/registrations_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Controller for registrations
+class Api::V1::Devise::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+    if !adult(resource.birth_date)
+      render json: { error: 'User must have 18 years old.' }, status: :bad_request
+    else
+      resource.save
+      if resource.persisted?
+        render json: resource, status: :created
+      else
+        render json: resource.errors, status: :bad_request
+      end
+    end
+  end
+
+  private
+
+  def adult(date)
+    date_of_birth = Date.strptime(date, '%d/%m/%Y')
+
+    return true if Date.today.year - date_of_birth.year > 18
+    return false if Date.today.year - date_of_birth.year < 18
+
+    return false if Date.today.month < date_of_birth.month
+    return true if Date.today.month > date_of_birth.month
+
+    Date.today.day >= date_of_birth.day if Date.today.month == date_of_birth.month
+  end
+end
--- app/controllers/api/v1/devise/volunteers/registrations_controller.rb
+++ app/controllers/api/v1/devise/volunteers/registrations_controller.rb
@@ -1,24 +1,8 @@
 # frozen_string_literal: true
 
 # Controller for Volunteers registrations
-class Api::V1::Devise::Volunteers::RegistrationsController < Devise::RegistrationsController
-  respond_to :json
+class Api::V1::Devise::Volunteers::RegistrationsController < Api::V1::Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-
-  # POST /resource
-  def create
-    build_resource(sign_up_params)
-    if !adult(resource.birth_date)
-      render json: { error: 'User must have 18 years old.' }, status: :bad_request
-    else
-      resource.save
-      if resource.persisted?
-        render json: resource, status: :created
-      else
-        render json: resource.errors, status: :bad_request
-      end
-    end
-  end
 
   protected
 
@@ -27,18 +11,5 @@ class Api::V1::Devise::Volunteers::RegistrationsController < Devise::Registratio
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password username name lastname birth_date
                                                          address type document_number document_type_id
                                                          avatar document_face_pic document_back_pic])
-  end
-
-  private
-
-  def adult(date)
-    date_of_birth = Date.strptime(date, '%d/%m/%Y')
-    return true if Date.today.year - date_of_birth.year > 18
-    return false if Date.today.year - date_of_birth.year < 18
-
-    return false if Date.today.month < date_of_birth.month
-    return true if Date.today.month > date_of_birth.month
-
-    Date.today.day >= date_of_birth.day if Date.today.month == date_of_birth.month
   end
 end
