fe97fd0cbe58cd99f123ec57b41526355255667a
Agustin Gazzano <agustingazzano107@gmail.com>
2020-10-22 20:05:45 -0300

    added seeds for notifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,15 @@ require 'csv'
 
 # to run use rake db:seed
 
+HELPEE_COUNT = 10
+VOLUNTEER_COUNT = 10
+
 # DOCUMENT TYPES
 
 ci_doc_type = DocumentType.new
 ci_doc_type.description = 'CI'
 ci_doc_type.save!
-puts ci_doc_type
+puts "created document type CI"
 
 # CATEGORIES
 
@@ -20,7 +23,7 @@ csv_categories.each do |category_row|
   category.description = category_row["description"]
   categories[category_row["description"]] = category
   category.save!
-  puts category
+  puts "created category " + category.description
 end
 
 # VOLUNTEERS
@@ -29,20 +32,22 @@ volunteer_list = []
 
 csv_volunteer_file = File.read(Rails.root.join('lib', 'seeds', 'volunteer.csv'))
 csv_volunteer = CSV.parse(csv_volunteer_file, :headers => true, :encoding => 'ISO-8859-1')
-csv_volunteer.each do |volunteer_row|
-  volunteer = Volunteer.new
-  volunteer.email = volunteer_row['email']
-  volunteer.password = volunteer_row['password']
-  volunteer.username = volunteer_row['username']
-  volunteer.name = volunteer_row['name']
-  volunteer.lastname = volunteer_row['lastname']
-  volunteer.birth_date = volunteer_row['birth_date']
-  volunteer.address = volunteer_row['address']
-  volunteer.document_number = volunteer_row['document_number']
-  volunteer.document_type_id = '1'
-  volunteer_list << volunteer
-  volunteer.save!
-  puts volunteer
+csv_volunteer.each_with_index do |volunteer_row, index|
+  if index < VOLUNTEER_COUNT
+    volunteer = Volunteer.new
+    volunteer.email = volunteer_row['email']
+    volunteer.password = volunteer_row['password']
+    volunteer.username = volunteer_row['username']
+    volunteer.name = volunteer_row['name']
+    volunteer.lastname = volunteer_row['lastname']
+    volunteer.birth_date = volunteer_row['birth_date']
+    volunteer.address = volunteer_row['address']
+    volunteer.document_number = volunteer_row['document_number']
+    volunteer.document_type_id = '1'
+    volunteer_list << volunteer
+    volunteer.save!
+    puts "created volunteer " + volunteer.name + " " + volunteer.lastname
+  end
 end
 
 # HELPEES
@@ -51,18 +56,20 @@ helpee_list = []
 
 csv_helpee_file = File.read(Rails.root.join('lib', 'seeds', 'helpee.csv'))
 csv_helpee = CSV.parse(csv_helpee_file, :headers => true, :encoding => 'ISO-8859-1')
-csv_helpee.each do |helpee_row|
-  helpee = Helpee.new
-  helpee.email = helpee_row['email']
-  helpee.password = helpee_row['password']
-  helpee.username = helpee_row['username']
-  helpee.name = helpee_row['name']
-  helpee.lastname = helpee_row['lastname']
-  helpee.birth_date = helpee_row['birth_date']
-  helpee.address = helpee_row['address']
-  helpee.save!
-  helpee_list << helpee
-  puts helpee
+csv_helpee.each_with_index do |helpee_row, index|
+  if index < HELPEE_COUNT
+    helpee = Helpee.new
+    helpee.email = helpee_row['email']
+    helpee.password = helpee_row['password']
+    helpee.username = helpee_row['username']
+    helpee.name = helpee_row['name']
+    helpee.lastname = helpee_row['lastname']
+    helpee.birth_date = helpee_row['birth_date']
+    helpee.address = helpee_row['address']
+    helpee.save!
+    helpee_list << helpee
+    puts "created helpee " + helpee.name + " " + helpee.lastname
+  end
 end
 
 # ORDERS
@@ -80,7 +87,7 @@ csv_order.each do |order_row|
   order.categories = [categories[order_row['category']]]
   order_list << order
   order.save!
-  puts order
+  puts "created order " + order.title
 end
 
 # ORDER REQUESTS
@@ -93,5 +100,20 @@ csv_order_request.each do |order_request_row|
   order_request.order = order_list[order_request_row['order'].to_i]
   order_request.order_request_status = order_request_row['status'].to_i
   order_request.save!
-  puts order_request
+  puts "created order request " + order_request.volunteer.username + " -> " + order_request.order.title
 end
+
+# NOTIFICATIONS
+
+csv = File.read(Rails.root.join('lib', 'seeds', 'notification.csv'))
+notifications = CSV.parse(csv, :headers => true, :encoding => 'ISO-8859-1')
+notifications.each_with_index do |notification_row, index|
+  notification = Notification.new
+  notification.title = notification_row['title']
+  notification.body = notification_row['body']
+  notification.status = notification_row['status'].to_i
+  notification.user = helpee_list[notification_row['user'].to_i]
+  notification.save!
+  print "saving notifications " + (index + 1).to_s + "/" + notifications.length.to_s + "\r"
+end
+puts
