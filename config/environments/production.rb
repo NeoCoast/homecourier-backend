5c58abb6ec3143f043c83fe9d6d8765a56b785d7
Andrés Collares <andres.collares@fing.edu.uy>
2020-10-27 16:51:19 -0300

    set environment variables for smtp host and cors origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'test-homecourier-backend.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV.fetch("SMTP_HOST") }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
