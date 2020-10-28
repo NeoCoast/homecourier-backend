374a0dbb22efde11a0ac6f4ad24552d90b534cf3
Andrés Collares <andres.collares@fing.edu.uy>
2020-10-27 23:58:08 -0300

    fix mailer params

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,23 +2,23 @@ class NotificationMailer < ApplicationMailer
     default from: "homecourierfing@gmail.com"
 
     def order_accepted_email(user, order)
-        @user = user
-        @order = order
+        @user = params[:user]
+        @order = params[:order]
         mail(to: @user.email, subject: 'Su pedido ha sido aceptado')
     end
     def order_in_process_email(user, order)
-        @user = user
-        @order = order
+        @user = params[:user]
+        @order = params[:order]
         mail(to: @user.email, subject: 'Su pedido está en proceso')
     end
     def order_finished_email(user, order)
-        @user = user
-        @order = order
+        @user = params[:user]
+        @order = params[:order]
         mail(to: @user.email, subject: 'Su pedido ha terminado')
     end
     def order_cancelled_email(user, order)
-        @user = user
-        @order = order
+        @user = params[:user]
+        @order = params[:order]
         mail(to: @user.email, subject: 'Su pedido fue cancelado')
     end
 end
