5e83f3bc35d36c34ce178cf6917a68b541ec3843
Diego Wins <diegow93@gmail.com>
2020-11-07 12:56:59 -0300

    Switch Geocoder to Google Places Search API.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,9 +3,9 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  lookup: :nominatim, # name of geocoding service (symbol)
+  lookup: :google_places_search, # name of geocoding service (symbol)
   ip_lookup: :ipinfo_io, # name of IP address geocoding service (symbol)
-  # api_key: nil,               # API key for geocoding service
+  api_key: AIzaSyA2noh_s_i7IxYWQ1vsC0MwYWi1nDWFkDM, # API key for geocoding service
 
   # Calculation options
   units: :km                 # :km for kilometers or :mi for miles
