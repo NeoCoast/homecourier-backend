3677abae39d9eea7d3246434a58e33d527d5289c
Diego Wins <diegow93@gmail.com>
2020-11-07 13:31:48 -0300

    Google Places Search on encrypted file.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -5,7 +5,7 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
   lookup: :google_places_search, # name of geocoding service (symbol)
   ip_lookup: :ipinfo_io, # name of IP address geocoding service (symbol)
-  api_key: AIzaSyA2noh_s_i7IxYWQ1vsC0MwYWi1nDWFkDM, # API key for geocoding service
+  api_key: Rails.application.credentials.google_places_search, # API key for geocoding service
 
   # Calculation options
   units: :km                 # :km for kilometers or :mi for miles
