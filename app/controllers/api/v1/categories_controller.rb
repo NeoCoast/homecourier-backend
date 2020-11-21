99608fe404cc1b86b3f1432eb3725ce29b701497
Mauro Pintado <m.pintado1994@gmail.com>
2020-11-21 13:07:48 -0300

    deleted endpoints create and delete category

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,32 +2,11 @@
 
 # CategoriesController
 class Api::V1::CategoriesController < ApplicationController
-  before_action :load_category, only: %i[show destroy]
-
-  def create
-    @category = Category.create!(category_params)
-  end
-
   def index
     @categories = Category.all
   end
 
   def show
-    @category
-  end
-
-  def destroy
-    @category.destroy
-    head :ok
-  end
-
-  private
-
-  def category_params
-    params.permit(:description)
-  end
-
-  def load_category
     @category = Category.find(params[:id])
   end
 end
